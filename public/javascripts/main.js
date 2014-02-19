06536062d184e7fa2d7c0b010af41756a39f25ee
Andrey Lushnikov <aslushnikov@gmail.com>
2014-02-20 02:58:59 +0400

    added training mode

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,3 +14,35 @@ $(document).ready(function() {
         });
     });
 });
+
+// training script
+$(document).ready(function() {
+    // this script is only for training url
+    if (!window.words)
+        return;
+    roll();
+    $(".next-word").click(function() {
+        roll();
+    });
+
+    function roll() {
+        var section = $(".word-block");
+        // get the form of the event target
+        var orig = section.find("textarea[name=original]");
+        var trans = section.find("textarea[name=translation]");
+        window.currentWord = window.words[(Math.random() * window.words.length)|0];
+        if (window.location.hash === "#reverse") {
+            orig.val("");
+            trans.val(window.currentWord.translation);
+        } else {
+            orig.val(window.currentWord.original);
+            trans.val("");
+        }
+    }
+    $("textarea[name=original]").click(function() {
+        $(this).val(window.currentWord.original);
+    });
+    $("textarea[name=translation]").click(function() {
+        $(this).val(window.currentWord.translation);
+    });
+});
