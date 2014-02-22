07530c9fa2f3972446a9cebaeb37e05f505b2589
Andrey Lushnikov <aslushnikov@gmail.com>
2014-02-23 02:26:37 +0400

    shuffling words instead of pure random

--- public/javascripts/train.js
+++ public/javascripts/train.js
@@ -1,30 +1,53 @@
 $(document).ready(function() {
-    // this script is only for training url
-    if (!window.words || !window.words.length)
+    if (!window.bootstrapWords || !window.bootstrapWords.length)
         return;
-    roll();
     $(".next-word").click(function() {
         roll();
     });
+
+    var words = window.bootstrapWords;
+    var currentWordIndex = -1;
+    shuffle(words);
+    roll();
+
+    function shuffle(words) {
+        for (var i = 0; i < words.length; ++i) {
+            var position = (Math.random() * (words.length - i))|0;
+            var tmp = words[i];
+            words[i] = words[position + i];
+            words[position + i] = tmp;
+        }
+    }
+
+    function currentWord() {
+        return words[currentWordIndex];
+    }
+
+    function nextWord() {
+        if (++currentWordIndex >= words.length) {
+            currentWordIndex = 0;
+            shuffle(words);
+        }
+    }
 
     function roll() {
         var section = $(".word-block");
         // get the form of the event target
         var orig = section.find("textarea[name=original]");
         var trans = section.find("textarea[name=translation]");
-        window.currentWord = window.words[(Math.random() * window.words.length)|0];
+        nextWord();
         if (window.location.hash === "#reverse") {
             orig.val("");
-            trans.val(window.currentWord.translation);
+            trans.val(currentWord().translation);
         } else {
-            orig.val(window.currentWord.original);
+            orig.val(currentWord().original);
             trans.val("");
         }
     }
     $("textarea[name=original]").click(function() {
-        $(this).val(window.currentWord.original);
+        $(this).val(currentWord().original);
     });
     $("textarea[name=translation]").click(function() {
-        $(this).val(window.currentWord.translation);
+        $(this).val(currentWord().translation);
     });
 });
