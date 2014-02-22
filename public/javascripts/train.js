e2be44cf116c7c5cffe5cff2826d2954d2173c58
Andrey Lushnikov <aslushnikov@gmail.com>
2014-02-23 02:04:37 +0400

    a bit more refactoring

--- public/javascripts/train.js
+++ public/javascripts/train.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-    $("textarea[name=original]").focus();
-    $(".submit-word").click(function() {
-        var btn = $(this);
-        btn.addClass("disabled");
-        // get the form of the event target
-        var form = $(btn).parents('form:first');
-        // send ajax request
-        $.post(form.attr("action"), form.serialize())
-        .always(function(err, data) {
-            console.log(arguments);
-            // clear all text area fields
-            form.find("textarea").val('');
-            btn.removeClass("disabled");
-            $("textarea[name=original]").focus();
-        });
-    })
-    $("#create-word").keypress(function(e) {
-        if(e.which == 13) {
-            jQuery(this).blur();
-            jQuery('.submit-word').focus().click();
-        }
-    });
-});
-
-// training script
-$(document).ready(function() {
     // this script is only for training url
     if (!window.words || !window.words.length)
         return;
