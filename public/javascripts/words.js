78ef3b492dbf32ca632e53a6c863eff9c773d50a
Andrey Lushnikov <aslushnikov@gmail.com>
2014-04-03 01:30:56 +0400

    support remove word functionality

--- public/javascripts/words.js
+++ public/javascripts/words.js
@@ -5,3 +5,31 @@ $(document).ready(function() {
     }
 });
 
+$(document).ready(function() {
+    $(".dictionary").hammer().on("hold", function(e) {
+        var entry = $(e.target).closest(".entry");
+        var word = entry.find(".original").text();
+        var shouldRemove = confirm("Remove word '" + word + "'?");
+        if (!shouldRemove)
+            return;
+        var wordId = entry.attr("data-word-id");
+        removeWord(wordId, onWordRemoved);
+
+        function onWordRemoved()
+        {
+            entry.slideUp("fast");
+        }
+    });
+})
+
+function removeWord(wordId, callback)
+{
+    $.post("/word/remove/" + wordId)
+    .done(function() {
+        callback();
+    })
+    .fail(function(obj, err, errDescr) {
+        stub.failure("Error: " + errDescr);
+    })
+
+}
