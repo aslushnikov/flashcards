cd8619f86c67292628645b2b7a3eccd3edb5ae7e
Andrey Lushnikov <aslushnikov@gmail.com>
2014-04-14 01:50:59 +0400

    single css for all screens

--- gulpfile.js
+++ gulpfile.js
@@ -45,8 +45,9 @@ gulp.task("css-prefix", function() {
     .pipe(gulp.dest("./build/css"))
 });
 
-gulp.task("screen/words/css", ["css-prefix"], function() {
+gulp.task("build/css/words", ["css-prefix"], function() {
     gulp.src([
+        "build/css/normalize.css",
         "build/css/common.css",
         "build/css/screen-words.css",
         "build/css/title.css",
@@ -56,7 +57,64 @@ gulp.task("screen/words/css", ["css-prefix"], function() {
     .pipe(gulp.dest("./public/stylesheets/"))
 });
 
-gulp.task("screen/words/js", function() {
+gulp.task("build/css/login", ["css-prefix"], function() {
+    gulp.src([
+        "build/css/normalize.css",
+        "build/css/common.css",
+        "build/css/login.css",
+    ])
+    .pipe(concat("login.css"))
+    .pipe(gulp.dest("./public/stylesheets/"))
+});
+
+gulp.task("build/css/train-settings", ["css-prefix"], function() {
+    gulp.src([
+        "build/css/normalize.css",
+        "build/css/common.css",
+        "build/css/title.css",
+        "build/css/train-settings.css",
+    ])
+    .pipe(concat("train-settings.css"))
+    .pipe(gulp.dest("./public/stylesheets/"))
+});
+
+gulp.task("build/css/train", ["css-prefix"], function() {
+    gulp.src([
+        "build/css/normalize.css",
+        "build/css/common.css",
+        "build/css/title.css",
+        "build/css/train.css",
+    ])
+    .pipe(concat("train.css"))
+    .pipe(gulp.dest("./public/stylesheets/"))
+});
+
+
+gulp.task("build/css/menu", ["css-prefix"], function() {
+    gulp.src([
+        "build/css/normalize.css",
+        "build/css/common.css",
+        "build/css/menu.css",
+        "build/css/screen-menu.css",
+    ])
+    .pipe(concat("menu.css"))
+    .pipe(gulp.dest("./public/stylesheets/"))
+});
+
+gulp.task("build/css/add-word", ["css-prefix"], function() {
+    gulp.src([
+        "build/css/normalize.css",
+        "build/css/common.css",
+        "build/css/title.css",
+        "build/css/new-word.css",
+        "build/css/stub.css",
+        "build/css/tagcloud.css",
+    ])
+    .pipe(concat("add-word.css"))
+    .pipe(gulp.dest("./public/stylesheets/"))
+});
+
+gulp.task("build/js/words", function() {
     gulp.src([
         "js/Flash.js",
         "js/Word.js",
@@ -68,22 +126,28 @@ gulp.task("screen/words/js", function() {
     .pipe(gulp.dest('./public/javascripts'))
 });
 
-gulp.task("rebuild/css", [
-    "screen/words/css",
+gulp.task("build/css", [
+    "build/css/words",
+    "build/css/login",
+    "build/css/menu",
+    "build/css/add-word",
+    "build/css/train-settings",
+    "build/css/train",
 ]);
 
-gulp.task("rebuild/js", [
-    "screen/words/js",
+gulp.task("build/js", [
+    "build/js/words",
 ]);
+
+gulp.task("build", ["build/js", "build/css"]);
 
 // Rerun the task when a file changes
 gulp.task("watch", function() {
-    gulp.watch("css/*.css", ["rebuild/css"]);
-    gulp.watch("js/*.js", ["rebuild/js"]);
+    gulp.watch("css/*.css", ["build/css"]);
+    gulp.watch("js/*.js", ["build/js"]);
 });
 
-gulp.task("default", ["watch"], function() {
+gulp.task("default", ["build", "watch"], function() {
     nodemon({ script: 'app.js', ext: 'html js', ignore: ['public/', 'js/', 'css/'] })
     .on('restart', [])
-
 });
